1394dc9746ada32df399e67b11b98fbb4fd7f36f
Islam412 <islam.124.hamdy@gmail.com>
2024-01-24 17:12:58 +0200

    update fireware

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from . import views
+from core.views import index
 
+
+app_name = 'core'
 
 urlpatterns = [
-    path('',views.index)
+    path('',index),
 ]
--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,6 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from core.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
