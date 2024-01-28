075338c16b22b8e4d11f83641653d86976cf76c9
Islam412 <islam.124.hamdy@gmail.com>
2024-01-28 05:05:21 +0200

    configure Django Admin to model User Custom Admin

--- userauths/admin.py
+++ userauths/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from userauths.models import User , Profile 
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserCustomAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'username', 'email', 'gender']
 
 
@@ -10,5 +10,5 @@ class ProfileAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'user', 'verified']
 
 
-admin.site.register(User, UserAdmin)
+admin.site.register(User, UserCustomAdmin)
 admin.site.register(Profile, ProfileAdmin)
