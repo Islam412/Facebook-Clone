2979c73b7777833bfc346597da22c5cf166b3cb5
Islam412 <islam.124.hamdy@gmail.com>
2024-01-28 05:00:44 +0200

    configure Django Admin to model Profile

--- userauths/admin.py
+++ userauths/admin.py
@@ -6,6 +6,9 @@ class UserAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'username', 'email', 'gender']
 
 
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'user', 'verified']
 
-admin.site.register(User,UserAdmin)
-admin.site.register(Profile)
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile, ProfileAdmin)
