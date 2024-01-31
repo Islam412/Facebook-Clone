cb9a5a7ffb1b34da696ca5a4da602c52ad22ea14
Islam412 <islam.124.hamdy@gmail.com>
2024-01-31 03:47:35 +0200

    create form and create view RegisterViewp1

--- userauths/views.py
+++ userauths/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+from forms import UserCreationForm
+
+
+class RegisterView(request):
+    if request.user.is_authenticated:
+        messages.warning(request, 'You are registered!')
+        return render('core:feed')
+    
+    form = UserCreationForm(request.POST or None)
