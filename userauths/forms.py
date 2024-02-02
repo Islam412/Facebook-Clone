31472b1be567f433ce5595570e13b6f101abe1c1
Islam412 <islam.124.hamdy@gmail.com>
2024-02-03 01:38:14 +0200

    desing form UserRegisterForm in userauths app p2

--- userauths/forms.py
+++ userauths/forms.py
@@ -4,6 +4,12 @@ from userauths.models import User
 
 
 class UserRegisterForm(UserCreationForm):
+    full_name = forms.CharField(widget=forms.TextInput(attrs={'class': '', 'id': "", 'placeholder':'Full Name'}), max_length=100, required=True)
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': '', 'id': "", 'placeholder':'Username'}), max_length=100, required=True)
+    phone = forms.CharField(widget=forms.TextInput(attrs={'class': '', 'id': "", 'placeholder':'Mobile No.'}), max_length=100, required=True)
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': '' , 'id': "", 'placeholder':'Email Address'}), required=True)
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={'id': "", 'placeholder':'Password'}), required=True)
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={'id': "", 'placeholder':'Confirm Password'}), required=True)
     
     class Meta:
         model = User
