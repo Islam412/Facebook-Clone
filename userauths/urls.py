9db60b7aaef54e95b89ea2f101f43a2d49e94fae
Islam412 <islam.124.hamdy@gmail.com>
2024-02-02 02:22:13 +0200

    created RegisterView in views at userauths and created urls in userauths

---
+++ userauths/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from userauths import views
+
+
+app_name = 'userauths'
+
+
+urlpatterns = [
+    path('sign-up/', )
+]
--- userauths/views.py
+++ userauths/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.contrib.auth import authenticate, login
 
 from forms import UserCreationForm
+from userauths.models import Profile
 
 
 class RegisterView(request):
@@ -10,3 +12,26 @@ class RegisterView(request):
         return render('core:feed')
     
     form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        full_name = form.cleaned_data.get('full_name')
+        phone = form.cleaned_data.get('phone')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+
+        user = authenticate(email=email,password=password)
+        login(request, user)
+
+        
+        profile = Profile.objects.get(user=request.user)
+        profile.full_name = full_name
+        profile.phone = phone
+        profile.save()
+
+        messages.success(request,f'Hi {full_name}. Your account was created successfully.')
+        return render('core:feed')
+    
+    context = {
+        'form':form
+    }
+    return render(request, 'userauths/sign-up.html',context)
