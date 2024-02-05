f37b2d1c12c2aab402d0881b83b263816091ab53
Islam412 <islam.124.hamdy@gmail.com>
2024-02-05 22:09:33 +0200

    update code

--- userauths/models.py
+++ userauths/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.text import slugify
 from django.db.models.signals import post_save
+from django.contrib.auth.models import AbstractUser
+
 
 
 from PIL import Image
