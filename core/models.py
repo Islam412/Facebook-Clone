33a6a4769e45158aec98f61cf7d51a4d852e97cc
Islam412 <islam.124.hamdy@gmail.com>
2024-02-15 03:34:56 +0200

    desing models GroupPost p1

--- core/models.py
+++ core/models.py
@@ -189,3 +189,33 @@ class Group(models.Model):
     
 
 
+class GroupPost(models.Model):
+    group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    title = models.CharField(max_length=500, blank=True, null=True)
+    image = models.ImageField(upload_to=user_directory_path, blank=True, null=True)
+    video = models.FileField(upload_to=user_directory_path, blank=True, null=True)
+    visibility = models.CharField(max_length=100, choices=VISIBILITY, default='Everyone')
+    pid = ShortUUIDField(length=7, max_length=25, alphabet="abcdefghijklmnopqrstuvwxyz1234567890")
+    likes = models.ManyToManyField(User, blank=True, related_name="group_post_likes")
+    active = models.BooleanField(default=True)
+    slug = models.SlugField(unique=True)
+    views = models.PositiveIntegerField(default=0)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        if self.title:
+            return self.title
+        else:
+            return self.user.username
+        
+    def save(self, *args, **kwargs):
+        uuid_key = shortuuid.uuid()
+        uniqueid = uuid_key[:2]
+        if self.slug == "" or self.slug == None:
+            self.slug = slugify(self.title) + '-' + uniqueid
+
+        super(GroupPost, self).save(*args, **kwargs)
+    
+    def thumbnail(self):
+        return mark_safe('<img src="/media/%s" width="50" height="50" object-fit:"cover" style="border-radius: 5px;" />' % (self.image))
