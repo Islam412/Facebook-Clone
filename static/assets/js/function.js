30e362aafa5b3a24fb9c64fcacb34865a74dd382
Islam412 <islam.124.hamdy@gmail.com>
2024-02-26 01:57:52 +0200

    update code in function.js

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -36,11 +36,11 @@ $(document).ready(function(){
                 //     <div class="flex justify-between items-center lg:p-4 p-2.5">\
                 //         <div class="flex flex-1 items-center space-x-4">\
                 //             <a href="#">\
-                //                 <img src="' + res.posts.profile_image + '" style="width: 40px; height: 40px;" class="bg-gray-200 border border-white rounded-full w-10 h-10" />\
+                //                 <img src="' + res.post.profile_image + '" style="width: 40px; height: 40px;" class="bg-gray-200 border border-white rounded-full w-10 h-10" />\
                 //             </a>\
                 //             <div class="flex-1 font-semibold capitalize">\
-                //                 <a href="#" class="text-black dark:text-gray-100">' + res.posts.full_name + '</a>\
-                //                 <div class="text-gray-700 flex items-center space-x-2">' + res.posts.date + ' ago \
+                //                 <a href="#" class="text-black dark:text-gray-100">' + res.post.full_name + '</a>\
+                //                 <div class="text-gray-700 flex items-center space-x-2">' + res.post.date + ' ago \
                 //                     <ion-icon name="story-time"></ion-icon>\
                 //                 </div>\
                 //             </div>\
@@ -83,19 +83,19 @@ $(document).ready(function(){
                 //     </div>\
                 //     <div uk-lightbox>\
                 //             <div class="p-5 pt-0 border-b dark:border-gray-700 pb-3">\
-                //                 ' + res.posts.title + '\
+                //                 ' + res.post.title + '\
                 //             </div>\
                 //         <div class="grid grid-cols-2 gap-2 px-5">\
                 //             <!-- Show thumnnail -->\
-                //             <a href="' + res.posts.image_url + '" class="col-span-2">  \
-                //                 <img src="' + res.posts.image_url + '" style="width: 100%; height: 300px; object-fit: cover;" alt="" class="rounded-md w-full lg:h-76 object-cover">\
+                //             <a href="' + res.post.image_url + '" class="col-span-2">  \
+                //                 <img src="' + res.post.image_url + '" style="width: 100%; height: 300px; object-fit: cover;" alt="" class="rounded-md w-full lg:h-76 object-cover">\
                 //             </a>\
                 //         </div>\
                 //     </div>\
                 //     <div class="p-4 space-y-3">\
                 //         <div class="flex space-x-4 lg:font-bold">\
                 //             <a  class="flex items-center space-x-2  text-blue-500" style="cursor: pointer;" >\
-                //                 <div class="p-2 rounded-full like-btn'+res.posts.id+' text-black " id="like-btn" data-like-btn="'+res.posts.id+'">\
+                //                 <div class="p-2 rounded-full like-btn'+res.post.id+' text-black " id="like-btn" data-like-btn="'+res.post.id+'">\
                 //                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" width="22" height="22" class="dark:text-blue-100">\
                 //                         <path d="M2 10.5a1.5 1.5 0 113 0v6a1.5 1.5 0 01-3 0v-6zM6 10.333v5.43a2 2 0 001.106 1.79l.05.025A4 4 0 008.943 18h5.416a2 2 0 001.962-1.608l1.2-6A2 2 0 0015.56 8H12V4a2 2 0 00-2-2 1 1 0 00-1 1v.667a4 4 0 01-.8 2.4L6.8 7.933a4 4 0 00-.8 2.4z" />\
                 //                     </svg>\
@@ -108,7 +108,7 @@ $(document).ready(function(){
                 //                         <path fill-rule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clip-rule="evenodd" />\
                 //                     </svg>\
                 //                 </div>\
-                //                 <div> <b><span id="comment-count'+res.posts.id+'">0</span></b> Comment</div>\
+                //                 <div> <b><span id="comment-count'+res.post.id+'">0</span></b> Comment</div>\
                 //             </a>\
                 //             <a href="#" class="flex items-center space-x-2 flex-1 justify-end">\
                 //                 <div class="p-2 rounded-full  text-black lg:bg-gray-100 dark:bg-gray-600">\
@@ -122,16 +122,16 @@ $(document).ready(function(){
                 //         <div class="flex items-center space-x-3 pt-2">\
                 //             \
                 //             <div class="dark:text-gray-100">\
-                //                 <strong><span id="like-count'+res.posts.id+'">0</span></strong> Likes\
+                //                 <strong><span id="like-count'+res.post.id+'">0</span></strong> Likes\
                 //             </div>\
                 //         </div>\
-                //         <div class="border-t py-4 space-y-4 dark:border-gray-600" id="comment-div'+res.posts.id+'">\
+                //         <div class="border-t py-4 space-y-4 dark:border-gray-600" id="comment-div'+res.post.id+'">\
                 //         </div>\
                 //             <a href="#" class="hover:text-blue-600 hover:underline">No Comments </a>\
                 //             <div class="bg-gray-100 rounded-full relative dark:bg-gray-800 border-t">\
-                //                 <input placeholder="Add your Comment..." id="comment-input'+res.posts.id+'" data-comment-input="'+res.posts.id+'" class="bg-transparent max-h-10 shadow-none px-5 comment-input'+res.posts.id+'">\
+                //                 <input placeholder="Add your Comment..." id="comment-input'+res.post.id+'" data-comment-input="'+res.post.id+'" class="bg-transparent max-h-10 shadow-none px-5 comment-input'+res.post.id+'">\
                 //                 <div class="-m-0.5 absolute bottom-0 flex items-center right-3 text-xl">\
-                //                     <a style="cursor: pointer;" id="comment-btn" class="comment-btn'+res.posts.id+'" data-comment-btn="'+res.posts.id+'">\
+                //                     <a style="cursor: pointer;" id="comment-btn" class="comment-btn'+res.post.id+'" data-comment-btn="'+res.post.id+'">\
                 //                         <ion-icon name="send-outline" class="hover:bg-gray-200 p-1.5 rounded-full"></ion-icon>\
                 //                     </a>\
                 //                 </div>\
