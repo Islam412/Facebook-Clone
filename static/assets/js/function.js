f8cf10490fc64fb70a09e275add284f12e245db8
Islam412 <islam.124.hamdy@gmail.com>
2024-02-28 06:40:00 +0200

    add ajax with comment on post

--- static/assets/js/function.js
+++ static/assets/js/function.js
@@ -184,12 +184,12 @@ $(document).ready(function(){
 
     // comment on post
     $(document).on("click", "#comment-btn", function(){
-        let id = $(this).attr("data-comment-btn")
+        let id = $(this).attr("data-comment-btn");
         let comment = $("#comment-input" + id).val();
-
+    
         console.log(id);
         console.log(comment);
-
+    
         $.ajax({
             url: "/comment_on_post/",
             dataType: "json",
@@ -199,10 +199,31 @@ $(document).ready(function(){
             },
             success: function(response){
                 console.log(response);
+    
+                let newComment = `
+                <div class="flex card shadow p-2">
+                    <div class="w-10 h-10 rounded-full relative flex-shrink-0">
+                        <img src="${response.data.profile_image}" alt="" class="absolute h-full rounded-full w-full">
+                    </div>
+                    <div>
+                        <div class="text-gray-700 py-2 px-3 rounded-md bg-gray-100 relative lg:ml-5 ml-2 lg:mr-12  dark:bg-gray-800 dark:text-gray-100">
+                            <p class="leading-6">${response.data.comment}</p>
+                            <div class="absolute w-3 h-3 top-3 -left-1 bg-gray-100 transform rotate-45 dark:bg-gray-800"></div>
+                        </div>
+                        <div class="text-sm flex items-center space-x-3 mt-2 ml-5">
+                            <a href="#" class="text-red-600"> <i class="uil-heart"></i> Love </a>
+                            <a href="#"> Replay </a>
+                            <span> ${response.data.date} </span>
+                        </div>
+                    </div>
+                </div>
+                `;
+                $("#comment-div" + id).prepend(newComment);
+                $("#comment-input" + id).val("");
             },
         });
-
     });
+    
 
 });
 
