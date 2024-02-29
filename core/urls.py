b4078eadaec7b6b47d2e0ba1729f9e9433450ed2
Islam412 <islam.124.hamdy@gmail.com>
2024-02-29 17:53:26 +0200

    call like_comment in url

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home, create_post, like_post, comment_on_post
+from .views import home, create_post, like_post, comment_on_post, like_comment
 
 
 app_name = 'core'
@@ -10,4 +10,5 @@ urlpatterns = [
     path('create_post/',create_post, name='create_post'),
     path('like_post/',like_post, name='like_post'),
     path('comment_on_post/',comment_on_post, name='comment_on_post'),
+    path('like_comment/',like_comment, name='like_comment'),
 ]
