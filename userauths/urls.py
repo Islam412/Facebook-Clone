20fdb455997914963a3234a22a36b0195102300e
Islam412 <islam.124.hamdy@gmail.com>
2024-03-20 17:56:07 +0200

    call my profile in urls

--- userauths/urls.py
+++ userauths/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('sign-up/', views.RegisterView, name='sign-up'),
     path('sign-in/', views.LoginView, name='sign-in'),
     path('user/sign-out/', views.LogoutView, name='sign-out'),
+    path('user/my-profile/', views.my_profile, name='my-profile'),
 ]
