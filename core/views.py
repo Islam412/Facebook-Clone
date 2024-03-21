fe5d7e495c3b166536f6d14b2216bc387cf07831
Islam412 <islam.124.hamdy@gmail.com>
2024-03-22 01:11:43 +0200

    add add_friend in views

--- core/views.py
+++ core/views.py
@@ -8,7 +8,8 @@ from django.contrib.auth.decorators import login_required
 
 
 import shortuuid
-from .models import Post, Comment, ReplyComment
+from .models import Post, Comment, ReplyComment, Friend, FriendRequest
+from userauths.models import User
 
 
 
@@ -184,4 +185,32 @@ def delete_reply_comment(request):
     }
     
     return JsonResponse({'data':data})
+
+
+
+def add_friend(request):
+    sender = request.user
+    receiver_id = request.GET['id']
+    bool = False
+    
+    if sender.id == int(receiver_id):
+        return JsonResponse({'error':'You cannot send a friend request to yourself'})
+    
+    receiver = User.objects.get(id=receiver_id)
+    
+    try:
+        friend_request = FriendRequest.objects.get(sender=sender, receiver=receiver)
+        if friend_request:
+            friend_request.delete()
+        bool=False
+        return JsonResponse({'error':'Cancelled', 'bool':bool})
+
+    except FriendRequest.DoesNotExist:
+        friend_request = FriendRequest(sender=sender, receiver=receiver)
+        friend_request.save()
+        bool = True
+        return JsonResponse({'success':'Sent', 'bool':bool})
+
+        
+        
     
