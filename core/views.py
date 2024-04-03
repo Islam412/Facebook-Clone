00ff4db2db3265a8d6c8a3f5e6b8142c5d3fa754
Islam412 <islam.124.hamdy@gmail.com>
2024-04-03 20:51:59 +0200

    desing accept_friend_request in views

--- core/views.py
+++ core/views.py
@@ -211,6 +211,22 @@ def add_friend(request):
         bool = True
         return JsonResponse({'success':'Sent', 'bool':bool})
 
-        
-        
+
+def accept_friend_request(request):
+    id = request.GET['id']
     
+    receiver = request.user
+    sender = User.objects.get(id=id)
+    
+    friend_request = FriendRequest.objects.filter(receiver=receiver, sender=sender).first()
+    
+    receiver.profile.friends.add(sender)
+    sender.profile.friends.add(receiver)
+    
+    friend_request.delete()
+    
+    data = {
+        'message' : 'Accepted',
+        'bool' : True,
+    }
+    return JsonResponse({"data":data})
