cfeff6fb7e082450e1225e214d3b24feafcbef2f
Islam412 <islam.124.hamdy@gmail.com>
2024-04-03 20:56:52 +0200

    call accept_friend_request in urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views import home, create_post, like_post, comment_on_post, like_comment, reply_comment, delete_comment, delete_reply_comment, post_detail, add_friend
-
+from .views import home, create_post, like_post, comment_on_post, like_comment, reply_comment, delete_comment, delete_reply_comment, post_detail, add_friend, accept_friend_request
 
 app_name = 'core'
 
@@ -18,4 +17,5 @@ urlpatterns = [
     path('delete_comment/',delete_comment, name='delete_comment'),
     path('delete_reply_comment/',delete_reply_comment, name='delete_reply_comment'),
     path('add_friend/',add_friend , name='add_friend'),
+    path('accept_friend_request/',accept_friend_request , name='accept_friend_request'),
 ]
