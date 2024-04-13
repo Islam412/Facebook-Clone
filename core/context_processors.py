9b38ef5dc43d79c5879f0f530278c8a650e35a46
Islam412 <islam.124.hamdy@gmail.com>
2024-04-13 02:01:09 +0200

    add notification in contexet_processor

--- core/context_processors.py
+++ core/context_processors.py
@@ -1,13 +1,19 @@
 # core/context_processors.py
 
-from core.models import FriendRequest
+from core.models import FriendRequest, Notification
 
 def my_context_processors(request):
     try:
         friend_request = FriendRequest.objects.filter(receiver=request.user).order_by("-id")
     except:
         friend_request = None
+        
+    try:
+        notification = Notification.objects.filter(user=request.user)
+    except:
+        notification = None
 
     return {
-        'friend_request': friend_request
+        'friend_request': friend_request,
+        'notification': notification,
     }
