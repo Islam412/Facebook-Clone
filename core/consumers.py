26134605f7b081452a81856fdf8dd724c6de0e2b
Islam Hamdy <islam.124.hamdy@gmail.com>
2024-04-20 03:46:20 +0200

    add chat and room chanels

---
+++ core/consumers.py
@@ -0,0 +1,21 @@
+from asgiref.sync import async_to_sync
+from channels.generic.websocket import WebsocketConsumer
+
+import json
+
+from userauths.models import User, Profile
+from core.models import ChatMessage
+
+
+class ChatConsumer(WebsocketConsumer):
+    def connect(self):
+        self.room_name = self.scope['url_rote']['kwargs']['room_name']
+        self.room_group_name = 'chat_%s' % self.room_name
+        
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name,
+            self.channel_name
+        )
+        self.accept()
+        
+    
