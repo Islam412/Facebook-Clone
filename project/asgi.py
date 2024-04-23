b06373e46c3d404e89f0594ac7d3a76928d48bfe
Islam Hamdy <islam.124.hamdy@gmail.com>
2024-04-23 17:48:00 +0200

    add asgi for applications

--- project/asgi.py
+++ project/asgi.py
@@ -1,18 +1,9 @@
-"""
-ASGI config for project project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
 import os
 import django
 from django.core.asgi import get_asgi_application
 
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'facebook_prj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
