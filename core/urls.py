c0c12f3113320a16964e6a45823124a7ea9669ac
Islam Hamdy <islam.124.hamdy@gmail.com>
2024-04-25 17:59:47 +0200

    call block_user in urls

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import home, create_post, like_post, comment_on_post, like_comment, reply_comment, delete_comment, delete_reply_comment, post_detail, add_friend, accept_friend_request, reject_friend_request, unfriend, inbox, inbox_detail
+from .views import home, create_post, like_post, comment_on_post, like_comment, reply_comment, delete_comment, delete_reply_comment, post_detail, add_friend, accept_friend_request, reject_friend_request, unfriend, inbox, inbox_detail, block_user
 
 app_name = 'core'
 
@@ -24,4 +24,5 @@ urlpatterns = [
     path('accept_friend_request/',accept_friend_request , name='accept_friend_request'),
     path('reject_friend_request/',reject_friend_request , name='reject_friend_request'),
     path('unfriend/',unfriend , name='unfriend'),
+    path('block_user/',block_user , name='block_user'),
 ]
